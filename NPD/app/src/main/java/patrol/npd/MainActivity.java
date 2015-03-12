ae627a27cc6c7f2cb84d0c43258e8016c8c3c6fa
shikhar0001 <godblees_only4u2@yahoo.com>
2015-03-12 00:44:04 -0500

    Revert "Revert "added database""
    This reverts commit 22c2bcd430748e968925a068f04bf3f22216bc06.

--- NPD/app/src/main/java/patrol/npd/MainActivity.java
+++ NPD/app/src/main/java/patrol/npd/MainActivity.java
@@ -1,6 +1,6 @@
 //abc
 //def
-/// nab
+
 package patrol.npd;
 
 import android.support.v7.app.ActionBarActivity;
@@ -13,11 +13,17 @@ import android.widget.EditText;
 import android.content.Intent;
 import android.widget.Toast;
 
+import database.LoginDataBaseAdapter;
+
 public class MainActivity extends ActionBarActivity {
+
+     LoginDataBaseAdapter DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // create a instance of SQLite Database
+        DB = new LoginDataBaseAdapter(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button login=(Button)findViewById(R.id.login_button);
@@ -26,17 +32,40 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v)
             {
                // Toast.makeText(getApplicationContext(), "login pressed", Toast.LENGTH_LONG).show();
-                EditText userName=(EditText)findViewById(R.id.userName);
-                EditText password=(EditText)findViewById(R.id.password);
+                final EditText userName=(EditText)findViewById(R.id.userName);
+                final EditText password=(EditText)findViewById(R.id.password);
                 String user=userName.getText().toString();
                 String pass=password.getText().toString();
-                if(user.equals("arvind") && pass.equals("pass"))
-                {
-                    Toast.makeText(getApplicationContext(), "in if", Toast.LENGTH_LONG).show();
-                    Intent loggedin=new Intent(MainActivity.this,User.class);
-                    loggedin.putExtra("user_name",user);
-                    startActivity(loggedin);
-                }
+
+                // fetch the Password from database for respective user name
+                String storedPassword = DB.GetSinlgeEntry(user);
+
+                if (pass.equals(storedPassword)) {
+                    Toast.makeText(MainActivity.this,
+                            "Congrats: Login Successfull", Toast.LENGTH_LONG)
+                            .show();
+
+                    // Set username to be used globally
+                    GlobalVariables globalVariable = (GlobalVariables) getApplicationContext();
+                    globalVariable.SetUserName(user);
+
+                }else {
+                        Toast.makeText(MainActivity.this,
+                                "User Name or Password does not match",
+                                Toast.LENGTH_LONG).show();
+                                user = "arvind";
+                                pass = "pass";
+                        {
+                           // Toast.makeText(getApplicationContext(), "in if", Toast.LENGTH_LONG).show();
+                            Intent loggedin =new Intent(MainActivity.this,User.class);
+                            loggedin.putExtra("user_name",user);
+                            loggedin.putExtra("password", pass);
+                            startActivity(loggedin);
+                        }
+                    }
+
+
+
             }
         });
     }
@@ -62,5 +91,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Close The Database
+        if (DB.db != null)
+            DB.db.close();
     }
 }
